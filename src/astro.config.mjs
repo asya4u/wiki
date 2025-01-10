4495f4e8291516b7aca77ed5694ecbda1ef1f298
LightChimera <artemilin347@gmail.com>
2025-01-11 00:21:54 +0300

    Пупупууу

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://asya4u.github.io",
+	base: "wiki",
 	integrations: [
 		starlight({
 			title: 'Asya docs',
