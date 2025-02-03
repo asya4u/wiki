cedf601f20101135bd568bbaab8f156c849b6512
webHikari <q5503212@gmail.com>
2025-02-03 22:30:45 +0300

    Начальника сказал пофиксить word-wrap в таблице и убрать блюр на хедере. Хихари сделал.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,5 @@
 
 export default new Map([
-["src/content/docs/guides/EventSystem.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2FEventSystem.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guides/Macros.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2FMacros.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/Configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2FConfiguration.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
